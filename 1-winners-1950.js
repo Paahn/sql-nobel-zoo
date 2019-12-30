dca71dfdb550939131ca17c079ae67ddc29ed25a
Panos Hatzinikolaou <45132887+Paahn@users.noreply.github.com>
2019-12-30 12:41:43 -0800

    Delete 1-winners-1950.js

--- 1-winners-1950.js
+++
@@ -1,6 +0,0 @@
-// query the database so you display Nobel prizes for 1950
-// our database is called nobel and it contains, the year, subject and name of the winners of the nobel prize.
-// check out the https://sqlzoo.net/ page for more details
-
-SELECT yr, subject, winner FROM nobel
-WHERE yr = 1950;
